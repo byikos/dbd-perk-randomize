1919bffaaf6aa83cbd798e1757780f14187e999e
Gianfranco Gioia <gianfranco.gioia00@gmail.com>
2025-07-19 02:42:32 -0400

    Actualización: perks visibles y contador dinámico

---
+++ perks-data.js
@@ -0,0 +1,245 @@
+const survivorPerks = [
+  {
+    "name": "Adrenaline",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/adrenaline.png"
+  },
+  {
+    "name": "Balanced Landing",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/balanced_landing.png"
+  },
+  {
+    "name": "Borrowed Time",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/borrowed_time.png"
+  },
+  {
+    "name": "Dead Hard",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/dead_hard.png"
+  },
+  {
+    "name": "Decisive Strike",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/decisive_strike.png"
+  },
+  {
+    "name": "Deliverance",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/deliverance.png"
+  },
+  {
+    "name": "Empathy",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/empathy.png"
+  },
+  {
+    "name": "Iron Will",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/iron_will.png"
+  },
+  {
+    "name": "Kindred",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/kindred.png"
+  },
+  {
+    "name": "Leader",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/leader.png"
+  },
+  {
+    "name": "No Mither",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/no_mither.png"
+  },
+  {
+    "name": "Self-Care",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/self-care.png"
+  },
+  {
+    "name": "Sprint Burst",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/sprint_burst.png"
+  },
+  {
+    "name": "Urban Evasion",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/urban_evasion.png"
+  },
+  {
+    "name": "Unbreakable",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/unbreakable.png"
+  },
+  {
+    "name": "Windows of Opportunity",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/windows_of_opportunity.png"
+  },
+  {
+    "name": "We'll Make It",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/well_make_it.png"
+  },
+  {
+    "name": "Prove Thyself",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/prove_thyself.png"
+  },
+  {
+    "name": "Resilience",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/resilience.png"
+  },
+  {
+    "name": "Deja Vu",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/deja_vu.png"
+  },
+  {
+    "name": "Lithe",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/lithe.png"
+  },
+  {
+    "name": "Quick & Quiet",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/quick_and_quiet.png"
+  },
+  {
+    "name": "Dance With Me",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/dance_with_me.png"
+  },
+  {
+    "name": "Head On",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/head_on.png"
+  },
+  {
+    "name": "Off the Record",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/off_the_record.png"
+  },
+  {
+    "name": "Object of Obsession",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/object_of_obsession.png"
+  },
+  {
+    "name": "Inner Focus",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/inner_focus.png"
+  },
+  {
+    "name": "Lucky Break",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/lucky_break.png"
+  },
+  {
+    "name": "Overcome",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/overcome.png"
+  },
+  {
+    "name": "Repressed Alliance",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/repressed_alliance.png"
+  }
+];
+
+const killerPerks = [
+  {
+    "name": "Barbecue & Chili",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/barbecue_and_chili.png"
+  },
+  {
+    "name": "Hex: Ruin",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/hex_ruin.png"
+  },
+  {
+    "name": "Hex: No One Escapes Death",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/hex_no_one_escapes_death.png"
+  },
+  {
+    "name": "Pop Goes the Weasel",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/pop_goes_the_weasel.png"
+  },
+  {
+    "name": "Corrupt Intervention",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/corrupt_intervention.png"
+  },
+  {
+    "name": "Sloppy Butcher",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/sloppy_butcher.png"
+  },
+  {
+    "name": "Tinkerer",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/tinkerer.png"
+  },
+  {
+    "name": "Overcharge",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/overcharge.png"
+  },
+  {
+    "name": "Deadlock",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/deadlock.png"
+  },
+  {
+    "name": "Jolt",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/jolt.png"
+  },
+  {
+    "name": "Thanatophobia",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/thanatophobia.png"
+  },
+  {
+    "name": "Scourge Hook: Pain Resonance",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/scourge_hook_pain_resonance.png"
+  },
+  {
+    "name": "A Nurse's Calling",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/a_nurses_calling.png"
+  },
+  {
+    "name": "Bamboozle",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/bamboozle.png"
+  },
+  {
+    "name": "Save the Best for Last",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/save_the_best_for_last.png"
+  },
+  {
+    "name": "Play with Your Food",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/play_with_your_food.png"
+  },
+  {
+    "name": "Hex: Devour Hope",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/hex_devour_hope.png"
+  },
+  {
+    "name": "Brutal Strength",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/brutal_strength.png"
+  },
+  {
+    "name": "Monitor & Abuse",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/monitor_and_abuse.png"
+  },
+  {
+    "name": "Coulrophobia",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/coulrophobia.png"
+  },
+  {
+    "name": "Dark Devotion",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/dark_devotion.png"
+  },
+  {
+    "name": "Dragon's Grip",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/dragons_grip.png"
+  },
+  {
+    "name": "Dying Light",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/dying_light.png"
+  },
+  {
+    "name": "Franklin's Demise",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/franklins_demise.png"
+  },
+  {
+    "name": "Infectious Fright",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/infectious_fright.png"
+  },
+  {
+    "name": "Make Your Choice",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/make_your_choice.png"
+  },
+  {
+    "name": "Nemesis",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/nemesis.png"
+  },
+  {
+    "name": "No Way Out",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/no_way_out.png"
+  },
+  {
+    "name": "Oppression",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/oppression.png"
+  },
+  {
+    "name": "Starstruck",
+    "img": "https://gigglingcorpse.com/dev/deadbydaylight/images/starstruck.png"
+  }
+];
