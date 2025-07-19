e28628dab50065e5d0c2cb6aec89d8d3609ec128
Gianfranco Gioia <gianfranco.gioia00@gmail.com>
2025-07-19 02:23:48 -0400

    Actualización: perks visibles y contador dinámico

---
+++ perks-data.js
@@ -0,0 +1,18 @@
+const survivorPerks = [
+    { name: "Adrenaline", img: "https://gigglingcorpse.com/dev/deadbydaylight/images/adrenaline.png" },
+    { name: "Dead Hard", img: "https://gigglingcorpse.com/dev/deadbydaylight/images/dead_hard.png" },
+    { name: "Sprint Burst", img: "https://gigglingcorpse.com/dev/deadbydaylight/images/sprint_burst.png" },
+    { name: "Borrowed Time", img: "https://gigglingcorpse.com/dev/deadbydaylight/images/borrowed_time.png" },
+    { name: "Iron Will", img: "https://gigglingcorpse.com/dev/deadbydaylight/images/iron_will.png" },
+    // ... seguir agregando todas las perks
+  ];
+  
+  const killerPerks = [
+    { name: "Hex: Ruin", img: "https://gigglingcorpse.com/dev/deadbydaylight/images/hex_ruin.png" },
+    { name: "Barbecue & Chili", img: "https://gigglingcorpse.com/dev/deadbydaylight/images/barbecue_and_chili.png" },
+    { name: "Hex: No One Escapes Death", img: "https://gigglingcorpse.com/dev/deadbydaylight/images/hex_no_one_escapes_death.png" },
+    { name: "Pop Goes the Weasel", img: "https://gigglingcorpse.com/dev/deadbydaylight/images/pop_goes_the_weasel.png" },
+    { name: "Corrupt Intervention", img: "https://gigglingcorpse.com/dev/deadbydaylight/images/corrupt_intervention.png" },
+    // ... seguir agregando todas las perks
+  ];
+  
